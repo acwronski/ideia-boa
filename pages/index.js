9a1a7e93ed0c158854446dec34de8582c99b97d3
antonio carlos wronski <54239754+acwronski@users.noreply.github.com>
2020-11-26 08:06:45 -0300

    adicionei proprietario

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,8 @@ function Home() {
         <div>
             <h1>Pagina Inicial</h1>
             <h2>Antonio Carlos Wronski</h2>
-            <h3>projetando</h3>
+            <h3>projetando um site de especies de orquidias</h3>
+            <h3>Proprietaria: Rosangela Oliveira</h3>
 
             <Contador/>
             <div>Teste</div>
