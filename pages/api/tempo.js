b6571dc8fce69dd8034bc943ed43c5c7dc626b38
antonio carlos wronski <54239754+acwronski@users.noreply.github.com>
2021-01-23 17:41:12 -0300

    adicionei nova

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,7 +1,9 @@
 async function tempo(request, response) {
+
+  const apiSecret = process.env.CONVERTKIT_API_SECRET;
   const dynamicDate = new Date();
 
-  const subscribersResponse = await fetch("https://api.convertkit.com/v3/subscribers?api_secret=ZJwLOhi01EvB90CIpsyw1e0fGRzPfw1Q")
+  const subscribersResponse = await fetch(`https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}`);
   const subscribersResponseJson = await subscribersResponse.json();
   const inscritos = subscribersResponseJson.total_subscribers;
 
@@ -10,5 +12,5 @@ async function tempo(request, response) {
       inscritos: inscritos
   });
 }
-
+ 
 export default tempo;
